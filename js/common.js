91a5ce469a8ba189caaa3eabe0e1b80eec23eee9
Maksym Marko <maximus21@mail.ru>
2016-03-02 16:16:44 +0200

    slider head is ready

--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
 	
+	//owl
+	$(".owl-carousel").owlCarousel({  	
+		loop : true,
+		autoplay : true,
+		autoplayTimeout : 5000,
+		items : 1,
+		smartSpeed : 1000,
+		center : true,
+		autoplaySpeed : 2000,
+		mouseDrag : false,
+		dots : true
+	});
 })
